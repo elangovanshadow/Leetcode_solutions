24d8a65c50b7005b25625b70d8671f672b6c8b68
elangovanshadow <140651648+elangovanshadow@users.noreply.github.com>
2025-04-03 20:26:36 +0530

    Time: 21 ms (35.37%), Space: 45.6 MB (8.1%) - LeetHub

---
+++ 2486-most-frequent-even-element/2486-most-frequent-even-element.java
@@ -0,0 +1,38 @@
+class Solution {
+    public int mostFrequentEven(int[] nums) {
+        HashMap<Integer, Integer> dic = new HashMap<Integer, Integer>();
+        for (int i = 0; i < nums.length; i++){
+            if (nums[i] % 2 == 0){
+                if(dic.get(nums[i])!=null){
+                    int val = dic.get(nums[i]);
+                    val++;
+                    dic.put(nums[i],val);
+                }
+                else{
+                    dic.put(nums[i],1);
+                }
+            }
+            
+        }
+        int max=-1;
+        int res = -1;
+        ArrayList<Integer> ans=new ArrayList<>();
+
+        for (Integer i : dic.keySet()) {
+            if(dic.get(i)>=max){
+                max=dic.get(i);
+            }
+        }
+        for (Integer i : dic.keySet()) {
+            if(dic.get(i)==max){
+                ans.add(i);
+            }
+        }
+
+        Collections.sort(ans);
+        if (ans.size() > 0){
+            return ans.get(0);
+        }
+        return -1;
+    }
+}
